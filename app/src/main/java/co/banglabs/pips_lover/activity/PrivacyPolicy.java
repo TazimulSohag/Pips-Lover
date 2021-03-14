b9ca16cbc06a4fd0eaee1fe6942a783d73226381
TazimulSohag <tazimulsohag@gmail.com>
2021-03-15 02:48:50 +0600

    icon added navigation header edited privacy policy layout added

---
+++ app/src/main/java/co/banglabs/pips_lover/activity/PrivacyPolicy.java
@@ -0,0 +1,26 @@
+package co.banglabs.pips_lover.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import co.banglabs.pips_lover.R;
+
+public class PrivacyPolicy extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_privacy_policy);
+
+
+
+    }
+
+
+
+
+
+
+}
