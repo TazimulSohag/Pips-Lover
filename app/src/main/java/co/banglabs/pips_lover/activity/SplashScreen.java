1e20ba90fddfc07d37034196fc4a6d28d6fb0f02
TazimulSohag <tazimulsohag@gmail.com>
2021-03-23 23:59:24 +0600

    splashscreen added

---
+++ app/src/main/java/co/banglabs/pips_lover/activity/SplashScreen.java
@@ -0,0 +1,41 @@
+package co.banglabs.pips_lover.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+
+import co.banglabs.pips_lover.R;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        Window w = getWindow(); // in Activity's onCreate() for instance
+        w.setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS, WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
+        getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
+
+
+
+        final Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+
+                Intent intent = new Intent(SplashScreen.this, Login.class);
+                startActivity(intent);
+                finish();
+
+            }
+        }, 2000);
+
+
+    }
+}
